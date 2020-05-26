a175d2dd6cfe3273a0710d772f5e51ac7e265085
tt <jorycn@163.com>
2020-05-26 16:53:32 +0800

    udpate kuaidi api config

--- config/kuaidi100.php
+++ config/kuaidi100.php
@@ -2,10 +2,10 @@
 
 return [
     'search' => [
-        'key' => env('KUAIDI_SEARCH_KEY', 'HfMOXqoe6967'),
-        'customer' => env('KUAIDI_SEARCH_CUSTOMER', '7000A7712AB4EA620D5E6A8F481171BF'),
+        'key' => env('KUAIDI_SEARCH_KEY', ''),
+        'customer' => env('KUAIDI_SEARCH_CUSTOMER', ''),
     ],
     'guest_code' => [
-        'key' => env('KUAIDI_TESTCODE', 'HfMOXqoe6967'),
+        'key' => env('KUAIDI_TESTCODE', ''),
     ]
 ];
